edf7e054fc4d32edbe2c4606b4acec1024a06837
yuripnp <yuripnp1992@gmail.com>
2025-04-23 20:38:31 -0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        String nome  = "Joao";
+        Double salario = 1000.0;
+        Boolean isento = false;
 
+        if(salario > 1000.0 && !isento) {
+            Double irrf = salario * 0.1;
+            System.out.println("IRRF: " + irrf);
+        } else if (!isento){
+            Double irrf = salario * 0.05;
+            System.out.println("IRRF: " + irrf);
+        } else {
+            System.out.println("Isento de IRRF");
+        }
     }
 }
